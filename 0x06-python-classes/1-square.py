f96822fce9cceb935ecf43f692cfb52d124d3199
0x00whitejsx <0x00whitejsx@gmail.com>
2024-05-08 13:05:29 +0100

    feat: class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module define a class square"""
+
+class Square:
+    """ class for squre """
+    def __init__(self, size):
+        """ Initialze class """
+        self.__size = size
